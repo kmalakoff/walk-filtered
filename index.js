b4cd2f442164326757c3a752afc428087f1862b7
Kevin Malakoff <kmalakoff@gmail.com>
2015-11-17 00:41:48 +0000

    Update to ES5

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var pathJoin = require('path').join;
 var EventEmitter = require('component-emitter');
+var assign = require('lodash.assign');
 
 // TODO: find a smaller, optimized version of eachSeries
 var async = require('async');
@@ -9,7 +10,7 @@ var each = async.eachSeries; // import each from 'each-series';
 function processPreStat(path, options, callback) {
   var fullPath = path ? pathJoin(options.cwd, path) : options.cwd;
 
-  fs.stat(fullPath, (err, stat) => {
+  fs.stat(fullPath, function(err, stat) {
     if (err) return callback(err);
 
     if (path && options.filter && options.filter(path, stat)) return callback();
@@ -17,11 +18,11 @@ function processPreStat(path, options, callback) {
     if (stat.isDirectory()) {
       !path || options.emitter.emit('directory', path, stat);
 
-      fs.readdir(fullPath, (err, names) => {
+      fs.readdir(fullPath, function(err, names) {
         if (err) return callback(err);
 
-        var paths = names.map((name) => path ? pathJoin(path, name) : name);
-        each(paths, (path, callback) => processPreStat(path, options, callback), callback);
+        var paths = names.map(function(name) { return path ? pathJoin(path, name) : name; });
+        each(paths, function(path, callback) { processPreStat(path, options, callback); }, callback);
       });
     }
     else {
@@ -34,19 +35,19 @@ function processPreStat(path, options, callback) {
 function processPostStat(path, options, callback) {
   var fullPath = path ? pathJoin(options.cwd, path) : options.cwd;
 
-  fs.stat(fullPath, (err, stat) => {
+  fs.stat(fullPath, function(err, stat) {
     if (err) return callback(err);
 
     if (stat.isDirectory()) {
       options.emitter.emit('directory', path, stat);
 
-      fs.readdir(fullPath, (err, names) => {
+      fs.readdir(fullPath, function(err, names) {
         if (err) return callback(err);
 
-        var paths = names.map((name) => path ? pathJoin(path, name) : name);
+        var paths = names.map(function(name) { return path ? pathJoin(path, name) : name; });
         if (options.filter) paths = paths.filter(options.filter);
         if (!paths.length) return callback();
-        each(paths, (path, callback) => processPostStat(path, options, callback), callback);
+        each(paths, function(path, callback) { processPostStat(path, options, callback); }, callback);
       });
     }
     else {
@@ -60,8 +61,8 @@ module.exports = function(cwd, options, callback) {
   if (!callback) { callback = options; options = {}; }
 
   var emitter = new EventEmitter()
-  options = (typeof options == 'function') ? {filter: options} : Object.assign({}, options);
-  Object.assign(options, {cwd, emitter});
+  options = (typeof options == 'function') ? {filter: options} : assign({}, options);
+  assign(options, {cwd, emitter});
 
   options.preStat ? processPreStat(null, options, callback) : processPostStat(null, options, callback);
   return emitter;
