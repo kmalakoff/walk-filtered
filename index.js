21a1319e770801e0366c5dc729a46d245f0c07e6
Kevin Malakoff <kmalakoff@gmail.com>
2015-12-07 10:08:12 -0800

    Added initial tests and performance tests

--- index.js
+++ index.js
@@ -3,44 +3,50 @@ var pathJoin = require('path').join;
 var relative = require('path').relative;
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
-var each = require('async-each-series');
+var eachSeries = require('async-each-series');
 
 function process(fullPath, options, callback) {
   var path = relative(options.cwd, fullPath); // the path to the link, file, or directory
-  if (options.lazyStats && !options.filter(path)) return callback(); // filter before stats
+  if (!options.preStat && !options.filter(path)) return callback(); // filter before stats
 
   // stat the path to the link, file, or directory
   options.stat(fullPath, function(err, stat) {
     if (err) return callback(err);
 
-    if (!options.lazyStats && !options.filter(path, stat)) return callback(); // filter with after and with stats
+    if (options.preStat && !options.filter(path, stat)) return callback(); // filter with after and with stats
 
     // a file or symlink
     if (!stat.isDirectory()) { options.emitter.emit('file', path, stat); return callback(); }
 
     // a directory
     options.emitter.emit('directory', path, stat);
-    fs.realpath(fullPath, function(err, realPath) {
+    options.fs.realpath(fullPath, function(err, realPath) {
       if (err) return callback(err);
 
-      fs.readdir(realPath, function(err, names) {
+      options.fs.readdir(realPath, function(err, names) {
         if (err) return callback(err);
 
         var fullPaths = names.map(function(name) { return pathJoin(realPath, name); });
-        each(fullPaths, function(fullPath, callback) { process(fullPath, options, callback); }, callback);
+        options.each(fullPaths, function(fullPath, callback) { process(fullPath, options, callback); }, callback);
       });
     });
   });
 }
 
 module.exports = function(cwd, options, callback) {
-  options = (typeof options == 'function') ? {filter: options} : options;
-  if (!options.filter) throw new Error('walk-filtered: missing filter option');
+  if (arguments.length === 2) { callback = options; options = {}; };
 
-  var emitter = new EventEmitter()
+  var emitter = new EventEmitter();
   fs.realpath(cwd, function(err, realCWD) {
     if (err) return emitter.emit('error', err);
-    options = assign({}, options, {cwd: realCWD, emitter, stat: options.lstat ? fs.lstat.bind(fs) : fs.stat.bind(fs)});
+
+    options = (typeof options == 'function') ? {filter: options} : assign({}, options);
+    options.emitter = emitter;
+    options.filter = options.filter || function() { return true; }
+    options.each = options.each || eachSeries;
+    options.fs = options.fs || fs;
+    options.stat = options.fs[options.stat || 'stat'].bind(options.fs);
+    options.cwd = realCWD;
     process(cwd, options, callback);
   })
   return emitter;
