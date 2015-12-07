732e5751ad74e8988ffc7de1761070bdede81fa1
Kevin Malakoff <kmalakoff@gmail.com>
2015-12-06 19:50:36 -0800

    Updating for symlinks

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var pathJoin = require('path').join;
+var pathRelative = require('path').relative;
 var EventEmitter = require('events').EventEmitter;
 var assign = require('lodash.assign');
 var eachSeries = require('async-each-series');
@@ -7,32 +8,37 @@ var eachSeries = require('async-each-series');
 function processPreStat(path, options, callback) {
   var fullPath = path ? pathJoin(options.cwd, path) : options.cwd;
 
-  fs.stat(fullPath, function(err, stat) {
+  fs.realpath(fullPath, function(err, realFullPath) {
     if (err) return callback(err);
 
-    if (path && options.filter && !options.filter(path, stat)) return callback();
+    options.stat(realFullPath, function(err, stat) {
+      if (err) return callback(err);
 
-    if (stat.isDirectory()) {
-      options.emitter.emit('directory', path, stat);
+      var relativePath = pathRelative(options.cwd, realFullPath);
+      if (relativePath && options.filter && !options.filter(relativePath, stat)) return callback();
 
-      fs.readdir(fullPath, function(err, names) {
-        if (err) return callback(err);
+      if (stat.isDirectory()) {
+        options.emitter.emit('directory', relativePath, stat);
 
-        var paths = names.map(function(name) { return path ? pathJoin(path, name) : name; });
-        eachSeries(paths, function(path, callback) { processPreStat(path, options, callback); }, callback);
-      });
-    }
-    else {
-      options.emitter.emit('file', path, stat);
-      callback();
-    }
+        fs.readdir(realFullPath, function(err, names) {
+          if (err) return callback(err);
+
+          var paths = names.map(function(name) { return relativePath ? pathJoin(relativePath, name) : name; });
+          eachSeries(paths, function(path, callback) { processPreStat(path, options, callback); }, callback);
+        });
+      }
+      else {
+        options.emitter.emit('file', relativePath, stat);
+        callback();
+      }
+    });
   });
 }
 
 function processPostStat(path, options, callback) {
   var fullPath = path ? pathJoin(options.cwd, path) : options.cwd;
 
-  fs.stat(fullPath, function(err, stat) {
+  options.stat(fullPath, function(err, stat) {
     if (err) return callback(err);
 
     if (stat.isDirectory()) {
@@ -58,9 +64,15 @@ module.exports = function(cwd, options, callback) {
   if (!callback) { callback = options; options = {}; }
 
   var emitter = new EventEmitter()
-  options = (typeof options == 'function') ? {filter: options} : assign({}, options);
-  assign(options, {cwd, emitter});
 
-  options.preStat ? processPreStat('', options, callback) : processPostStat('', options, callback);
+  fs.realpath(cwd, function(err, realCWD) {
+    if (err) return callback(err);
+
+    options = (typeof options == 'function') ? {filter: options} : assign({}, options);
+    assign(options, {cwd: realCWD, emitter, stat: fs.stat.bind(fs)});
+
+    options.preStat ? processPreStat('', options, callback) : processPostStat('', options, callback);
+  })
+
   return emitter;
 }
