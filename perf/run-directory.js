4bf000e6fe819b154a0a17b3fa131dbadb6a9c04
Kevin Malakoff <kmalakoff@gmail.com>
2015-12-09 21:13:07 -0800

    Updated API to remove EventEmitter and make the filter a mandatory second parameter.

--- perf/run-directory.js
+++ perf/run-directory.js
@@ -21,34 +21,34 @@ module.exports = function(dir, callback) {
 
   new Benchmark.Suite('Walk ' + relativeDir)
     .add('Serial (fs)', function(deferred) {
-      walk(dir, serialOptionsFn(fs), function(err) { err ? deferred.reject() : deferred.resolve();});
+      walk(dir, function() {}, serialOptionsFn(fs), function(err) { err ? deferred.reject() : deferred.resolve();});
     }, {defer: true})
 
     .add('Parallel (fs)', function(deferred) {
-      walk(dir, paralleOptionsFn(fs), function(err) { err ? deferred.reject() : deferred.resolve();});
+      walk(dir, function() {}, paralleOptionsFn(fs), function(err) { err ? deferred.reject() : deferred.resolve();});
     }, {defer: true})
     .add('Parallel (gfs)', function(deferred) {
-      walk(dir, paralleOptionsFn(gfs), function(err) { err ? deferred.reject() : deferred.resolve();});
+      walk(dir, function() {}, paralleOptionsFn(gfs), function(err) { err ? deferred.reject() : deferred.resolve();});
     }, {defer: true})
 
     .add('Parallel limit (fs, 10)', function(deferred) {
-      walk(dir, parallelLimitOptionsFn(fs, 10), function(err) { err ? deferred.reject() : deferred.resolve();});
+      walk(dir, function() {}, parallelLimitOptionsFn(fs, 10), function(err) { err ? deferred.reject() : deferred.resolve();});
     }, {defer: true})
     .add('Parallel limit (fs, 50)', function(deferred) {
-      walk(dir, parallelLimitOptionsFn(fs, 50), function(err) { err ? deferred.reject() : deferred.resolve();});
+      walk(dir, function() {}, parallelLimitOptionsFn(fs, 50), function(err) { err ? deferred.reject() : deferred.resolve();});
     }, {defer: true})
     .add('Parallel limit (fs, 100)', function(deferred) {
-      walk(dir, parallelLimitOptionsFn(fs, 100), function(err) { err ? deferred.reject() : deferred.resolve();});
+      walk(dir, function() {}, parallelLimitOptionsFn(fs, 100), function(err) { err ? deferred.reject() : deferred.resolve();});
     }, {defer: true})
 
     .add('Parallel limit (gfs, 10)', function(deferred) {
-      walk(dir, parallelLimitOptionsFn(gfs, 10), function(err) { err ? deferred.reject() : deferred.resolve();});
+      walk(dir, function() {}, parallelLimitOptionsFn(gfs, 10), function(err) { err ? deferred.reject() : deferred.resolve();});
     }, {defer: true})
     .add('Parallel limit (gfs, 50)', function(deferred) {
-      walk(dir, parallelLimitOptionsFn(gfs, 50), function(err) { err ? deferred.reject() : deferred.resolve();});
+      walk(dir, function() {}, parallelLimitOptionsFn(gfs, 50), function(err) { err ? deferred.reject() : deferred.resolve();});
     }, {defer: true})
     .add('Parallel limit (gfs, 100)', function(deferred) {
-      walk(dir, parallelLimitOptionsFn(gfs, 100), function(err) { err ? deferred.reject() : deferred.resolve();});
+      walk(dir, function() {}, parallelLimitOptionsFn(gfs, 100), function(err) { err ? deferred.reject() : deferred.resolve();});
     }, {defer: true})
 
     .on('start', function() { console.log('Comparing ' + this.name); })
--- test/lib/utils.js
+++ test/lib/utils.js
@@ -1,6 +1,41 @@
 var sysPath = require('path');
+var fs = require('fs-extra');
+var eachSeries = require('async-each-series');
 
-var DIR = sysPath.resolve(sysPath.join(__dirname, '..', '..', 'node_modules'));
+var DATA_DIR = sysPath.resolve(sysPath.join(__dirname, '..', 'data'));
 
-module.exports.beforeEach = function(callback) { callback(null, DIR); }
-module.exports.after = function(callback) { callback(); }
+var INIT_STRUCTURE = {
+  'file1': 'a',
+  'file2': 'b',
+  'dir1': null,
+  'dir2/file1': 'c',
+  'dir2/file2': 'd',
+  'dir3/dir4/file1': 'e',
+  'dir3/dir4/dir5': null,
+  'link1': '~dir3/dir4/file1',
+  'dir3/link2': '~dir2/file1'
+};
+
+module.exports.beforeEach = function(callback) {
+  fs.remove(DATA_DIR, function() {
+    eachSeries(Object.keys(INIT_STRUCTURE), function(path, callback) {
+      var fullPath = sysPath.join(DATA_DIR, path.split('/').join(sysPath.sep));
+
+      var contents = INIT_STRUCTURE[path];
+      if (!contents) return fs.mkdirs(fullPath, callback);
+
+      fs.mkdirs(sysPath.dirname(fullPath), function(err) {
+        if (err) return callback(err);
+        if (!contents.startsWith('~')) return fs.writeFile(fullPath, contents, 'utf8', callback);
+
+        var linkPath = sysPath.join(DATA_DIR, contents.slice(1).split('/').join(sysPath.sep));
+        fs.symlink(linkPath, fullPath, callback);
+      });
+    },
+    function(err) { err ? callback(err) : callback(null, DATA_DIR); });
+  });
+}
+
+module.exports.after = function(callback) {
+  fs.remove(DATA_DIR, function() { callback(); });
+}
