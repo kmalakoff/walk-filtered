738d5532780e1666bac559601535e54bd381fbfc
Kevin Malakoff <kmalakoff@gmail.com>
2019-02-07 06:48:35 -0800

    Removed references in tests to stats option. Added test for mis-traversal delete.

--- test/spec/everything.test.js
+++ test/spec/everything.test.js
@@ -5,7 +5,7 @@ var assert = chai.assert;
 var generate = require('fs-generate');
 var rimraf = require('rimraf');
 var sysPath = require('path');
-var fs = require('fs');
+var fs = require('fs-extra');
 
 var walk = require('../..');
 var statsSpys = require('../utils').statsSpys;
@@ -65,6 +65,27 @@ describe('walk everything', function() {
         assert.equal(spys.dir.callCount, 6);
         assert.equal(spys.file.callCount, 5);
         assert.equal(spys.link.callCount, 2);
+        callback();
+      }
+    );
+  });
+  it.only('Should handle a delete', function(callback) {
+    var spys = statsSpys();
+
+    walk(
+      DIR,
+      function(path) {
+        var stats = fs.lstatSync(sysPath.join(DIR, path));
+        spys(stats, path);
+
+        if (path === 'dir2/file1') fs.removeSync(sysPath.join(DIR, 'dir2'));
+        return true;
+      },
+      { concurrency: 1 },
+      function() {
+        assert.equal(spys.dir.callCount, 6);
+        assert.equal(spys.file.callCount, 4);
+        assert.equal(spys.link.callCount, 1);
         callback();
       }
     );
