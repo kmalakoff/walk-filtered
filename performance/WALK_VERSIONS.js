c226ec4a058371af4c943f753d2eff28cbdce59d
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-02 19:58:04 -0700

    Refactor for performance tests to compare current and previous versions

---
+++ performance/WALK_VERSIONS.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    version: 'latest',
+    walk: require('..'),
+  },
+  {
+    version: '0.13.0',
+    walk: require('walk-filtered-1.13.0'),
+  },
+  // {
+  //   version: '0.17.0',
+  //   walk: require('walk-filtered-1.17.0'),
+  // },
+];
