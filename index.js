0f62798c1ba5de2850b76234e6c66b7330b88d94
Kevin Malakoff <kevin@Kevins-MacBook-Air.local>
2020-04-05 12:18:24 -0700

    Refactored to use external iterator

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-module.exports = require('./iterator.js');
-// module.exports = require('./original.js')
+var Iterator = require('fs-iterator');
+var maximize = require('maximize-iterator');
+
+var DEFAULT_CONCURRENCY = 4096;
+
+module.exports = function walk(cwd, filter, options, callback) {
+  /* eslint-disable */
+  if (arguments.length === 3 && typeof options === 'function') {
+    callback = options;
+    options = {};
+  }
+  options = options || {};
+
+  var iterator = new Iterator(cwd, {
+    fs: options.fs,
+    stat: options.stat,
+    filter: filter,
+    async: options.async,
+  });
+
+  // choose between promise and callback API
+  if (typeof callback === 'function') return maximize(iterator, { concurrency: options.concurrency || DEFAULT_CONCURRENCY }, callback);
+  return new Promise(function (resolve, reject) {
+    walk(cwd, filter, options, function (err) {
+      err ? reject(err) : resolve();
+    });
+  });
+};
