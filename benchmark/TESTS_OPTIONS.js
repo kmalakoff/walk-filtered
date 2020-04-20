3dbef4038c59a42c0f01f3336960d97006eb7a1d
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-20 16:37:12 -0700

    Upgrade dependencies

--- benchmark/TESTS_OPTIONS.js
+++ benchmark/TESTS_OPTIONS.js
@@ -1,8 +1,8 @@
 // const CONCURRENCIES = [Infinity];
-const CONCURRENCIES = [100];
+// const CONCURRENCIES = [100];
 // const CONCURRENCIES = [1];
 // const CONCURRENCIES = [1, 1000, Infinity];
-// const CONCURRENCIES = [1, 100, 1000, Infinity];
+const CONCURRENCIES = [1, 100, 1000, Infinity];
 const TESTS_OPTIONS = [];
 
 // TESTS_OPTIONS.push({ name: `default` });
