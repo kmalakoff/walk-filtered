47f6124b611f0538b58582c79fbf1adbdc19f54c
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-23 20:30:03 -0700

    Update tests for latest fs-iterator

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var Iterator = require('fs-iterator');
 
-var DEFAULT_CONCURRENCY = 100;
-var NORMAL_FLOW_ERRORS = ['ENOENT', 'EPERM', 'EACCES', 'ELOOP'];
+module.exports = function walk(root, filter, options, callback) {
+  if (typeof root !== 'string') throw new Error('Directory is required');
+  if (typeof filter !== 'function') throw new Error('Filter is required');
 
-module.exports = function walk(cwd, filter, options, callback) {
   /* eslint-disable */
   if (arguments.length === 3 && typeof options === 'function') {
     callback = options;
@@ -14,15 +14,14 @@ module.exports = function walk(cwd, filter, options, callback) {
   if (typeof callback === 'function') {
     options = options || {};
 
-    var iterator = new Iterator(cwd, {
+    var iterator = new Iterator(root, {
       depth: options.depth === undefined ? Infinity : options.depth,
-      alwaysStat: options.alwaysStat,
-      fs: options.fs,
-      stat: options.stat,
+      alwaysStat: options.alwaysStat || false,
+      lstat: options.lstat || false,
       filter: filter,
-      async: options.async,
+      callbacks: options.callbacks || options.async,
       error: function (err) {
-        if (!~NORMAL_FLOW_ERRORS.indexOf(err.code)) return false;
+        if (!~Iterator.EXPECTED_ERRORS.indexOf(err.code)) return false;
         if (options.error) return options.error(err);
         return true;
       },
@@ -31,13 +30,17 @@ module.exports = function walk(cwd, filter, options, callback) {
     return iterator.forEach(
       function () {},
       {
-        concurrency: options.concurrency || DEFAULT_CONCURRENCY,
+        concurrency: options.concurrency || Infinity,
       },
-      callback
+      function forEachCallback(err) {
+        iterator.destroy();
+        iterator = null;
+        callback(err);
+      }
     );
   } else {
     return new Promise(function (resolve, reject) {
-      walk(cwd, filter, options, function (err) {
+      walk(root, filter, options, function (err) {
         err ? reject(err) : resolve();
       });
     });
