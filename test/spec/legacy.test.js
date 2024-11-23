8bfc21d847fe0d7dd4bb03c66fbac2d3f9e82e68
Kevin Malakoff <kmalakoff@gmail.com>
2024-11-22 19:42:58 -0800

    Upgrade modules

--- test/spec/legacy.test.js
+++ test/spec/legacy.test.js
@@ -1,14 +1,14 @@
-var assert = require('assert');
-var path = require('path');
-var rimraf = require('rimraf');
-var generate = require('fs-generate');
-var statsSpys = require('fs-stats-spys');
-var startsWith = require('starts-with');
+const assert = require('assert');
+const path = require('path');
+const rimraf = require('rimraf');
+const generate = require('fs-generate');
+const statsSpys = require('fs-stats-spys');
+const startsWith = require('starts-with');
 
-var walk = require('../..');
+const walk = require('walk-filtered');
 
-var TEST_DIR = path.resolve(path.join(__dirname, '..', '..', '.tmp', 'test'));
-var STRUCTURE = {
+const TEST_DIR = path.resolve(path.join(__dirname, '..', '..', '.tmp', 'test'));
+const STRUCTURE = {
   file1: 'a',
   file2: 'b',
   dir1: null,
@@ -20,66 +20,66 @@ var STRUCTURE = {
   'dir3/filelink2': '~dir2/file1',
 };
 
-describe('legacy', function () {
-  beforeEach(function (done) {
-    rimraf(TEST_DIR, function () {
+describe('legacy', () => {
+  beforeEach((done) => {
+    rimraf(TEST_DIR, () => {
       generate(TEST_DIR, STRUCTURE, done);
     });
   });
   after(rimraf.bind(null, TEST_DIR));
 
-  describe('async', function () {
-    it('Should filter everything under the root directory', function (done) {
-      var spys = statsSpys();
+  describe('async', () => {
+    it('Should filter everything under the root directory', (done) => {
+      const spys = statsSpys();
 
       walk(
         TEST_DIR,
-        function (entry, callback) {
+        (entry, callback) => {
           spys(entry.stats);
-          setTimeout(function () {
+          setTimeout(() => {
             callback(null, false);
           }, 10);
         },
         { async: true },
-        function () {
+        () => {
           assert.ok(spys.callCount, 1);
           done();
         }
       );
     });
 
-    it('Should filter everything under specific directories by relative path', function (done) {
-      var spys = statsSpys();
+    it('Should filter everything under specific directories by relative path', (done) => {
+      const spys = statsSpys();
 
       walk(
         TEST_DIR,
-        function (entry, callback) {
+        (entry, callback) => {
           spys(entry.stats);
-          setTimeout(function () {
+          setTimeout(() => {
             callback(null, entry.path !== 'dir2');
           }, 10);
         },
         { async: true },
-        function () {
+        () => {
           assert.ok(spys.callCount, 13 - 2);
           done();
         }
       );
     });
 
-    it('Should filter everything under specific directories by stats and relative path', function (done) {
-      var spys = statsSpys();
+    it('Should filter everything under specific directories by stats and relative path', (done) => {
+      const spys = statsSpys();
 
       walk(
         TEST_DIR,
-        function (entry, callback) {
+        (entry, callback) => {
           spys(entry.stats);
-          setTimeout(function () {
+          setTimeout(() => {
             callback(null, !entry.stats.isDirectory() || startsWith(entry.path, 'dir3/dir4'));
           }, 10);
         },
         { async: true },
-        function () {
+        () => {
           assert.ok(spys.callCount, 13 - 1);
           done();
         }
