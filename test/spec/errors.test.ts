74a728580523e7190faaa3a4545d92c626d28718
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-17 23:41:24 -0700

    Upgrade modules

--- test/spec/errors.test.ts
+++ test/spec/errors.test.ts
@@ -1,9 +1,9 @@
 import assert from 'assert';
-import path from 'path';
-import url from 'url';
 import generate from 'fs-generate';
+import path from 'path';
 import Pinkie from 'pinkie-promise';
 import rimraf2 from 'rimraf2';
+import url from 'url';
 
 // @ts-ignore
 import walk from 'walk-filtered';
