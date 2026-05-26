eda665a5045709037913d7951a55c47b4d4c5ecf
Kevin Malakoff <kmalakoff@gmail.com>
2026-05-26 14:23:41 -0700

    Fix broken types #2

--- src/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './types.ts';
-import type { Callback, FilterFunction, Options } from './types.ts';
-export default function walk(root: string, filter: FilterFunction): Promise<void>;
-export default function walk(root: string, filter: FilterFunction, options: Options): Promise<void>;
-export default function walk(root: string, filter: FilterFunction, callback: Callback): void;
-export default function walk(root: string, filter: FilterFunction, options: Options, callback: Callback): void;
