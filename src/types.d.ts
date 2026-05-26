a1cfdddbaedb97a47b50ae6e8db0e100ca21ba23
Kevin Malakoff <kmalakoff@gmail.com>
2026-05-26 14:23:41 -0700

    Fix broken types #2

--- src/types.d.ts
+++
@@ -1,4 +0,0 @@
-import type { IteratorOptions } from 'fs-iterator';
-export type { Entry, FilterFunction, IteratorOptions } from 'fs-iterator';
-export interface Options extends IteratorOptions {}
-export type Callback = (error?: Error) => void;
